2c20ebb1abae2335dcdde21386c0d8a856960192
Gabriel Fernandes Rosa Bojikian <gabriel.bojikian.dev@gmail.com>
2022-01-09 20:40:34 -0300

    Feature: Added process initiation

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import multiprocessing as mp
 import logging
 
 from configs import LOG_CONFIGS
-from processes import zmq_process
+from processes import fft_process, red_panda_process, zmq_process
 
 logging.basicConfig(**LOG_CONFIGS)
 logger = logging.getLogger(__name__)
@@ -11,12 +11,26 @@ logger = logging.getLogger(__name__)
 def main():
     try:
         # Create processes
-        zeromq_p = mp.Process(
+        mp_zeromq = mp.Process(
             target=zmq_process,
             # args=(),
         )
-        zeromq_p.start()
-        zeromq_p.join()
+
+        mp_red_panda = mp.Process(
+            target=red_panda_process,
+        )
+
+        mp_fft = mp.Process(
+            target=fft_process,
+        )
+
+        mp_zeromq.start()
+        mp_red_panda.start()
+        mp_fft.start()
+
+        mp_zeromq.join()
+        mp_red_panda.join()
+        mp_fft.join()
 
     except Exception as e:
         logger.warning(str(e))
--- src/processes/__init__.py
+++ src/processes/__init__.py
@@ -1,1 +1,3 @@
+from .fft_process import fft_process
+from .red_panda_process import red_panda_process
 from .zmq_process import zmq_process
