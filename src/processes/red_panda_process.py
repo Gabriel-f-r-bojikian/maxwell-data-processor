9589e4eb4566b1a1ee3c76d10cb8002edc30a6d7
hspadim <henrique@spadim.com.br>
2022-01-12 22:05:40 -0300

    Modify processes to display information on log messages

--- src/processes/red_panda_process.py
+++ src/processes/red_panda_process.py
@@ -1,7 +1,11 @@
+import logging
+
 from internal_queues import PARSED_DATA_QUEUE
+
+logger = logging.getLogger(__name__)
 
 
 def red_panda_process():
     while parsed_data := PARSED_DATA_QUEUE.get(block=True):
         # Send parsed data to Red Panda
-        print(parsed_data)
+        logger.debug(parsed_data)
